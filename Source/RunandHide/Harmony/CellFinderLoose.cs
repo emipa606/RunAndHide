4d399f9a5cb1eb3ac47dbf054676df3db5bcd5ae
Mlie <emipa606@users.noreply.github.com>
2020-03-14 21:02:14 +0100

    First publish

---
+++ Source/RunandHide/Harmony/CellFinderLoose.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using Verse;
+
+namespace RunandHide.Harmony
+{
+    [HarmonyPatch(typeof(CellFinderLoose), "GetFleeDestToolUser")]
+    public static class CellFinderLoosePatch
+    {
+        [HarmonyPrefix]
+        public static bool GetFleeDestToolUserPatch(ref IntVec3 __result, Pawn pawn, List<Thing> threats, float distance)
+        {
+            if(pawn.IsColonist)
+            {
+                Thing closestBunkerSpot = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForDef(ThingDefOfRunandHide.BunkerSpot), Verse.AI.PathEndMode.OnCell, TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn));
+                if(closestBunkerSpot != null)
+                {
+                    __result = closestBunkerSpot.Position;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/RunandHide/ThingDefOfRunandHide.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace RunandHide
+{
+    [DefOf]
+    public static class ThingDefOfRunandHide
+    {
+        public static ThingDef BunkerSpot;
+    }
+}
